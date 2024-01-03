dfe2a128e63de15e467a514579720f208ce67b46
gmd01bit <gmd01bit@gmail.com>
2024-01-03 13:04:07 +0200

    :memo: Adding comments and noting System F

--- types.pl
+++ types.pl
@@ -1,20 +1,28 @@
-% unify Expr is T given Ctx
+% Simply Typed Lambda Calculus
+% https://en.wikipedia.org/wiki/Simply_typed_lambda_calculus#Typing_rules
+%
+%! unify(?Ctx:list, +X, -T) is semidet.
 
-unify(X, int, _Ctx) :- integer(X).
+% (STLC-1)
+unify(Ctx, X, T) :- atomic(X), member(type(X, T), Ctx).
 
-unify(+(E1, E2), int, Ctx) :- unify(E1, int, Ctx), unify(E2, int, Ctx).
-unify(-(E1, E2), int, Ctx) :- unify(E1, int, Ctx), unify(E2, int, Ctx).
-unify(=:=(E1, E2), bool, Ctx) :- unify(E1, T, Ctx), unify(E2, T, Ctx).
-unify(=\=(E1, E2), bool, Ctx) :- unify(E1, T, Ctx), unify(E2, T, Ctx).
+% (STLC-2)
+unify(_Ctx, X, int) :- integer(X).
+unify(Ctx, +(E1, E2), int) :- unify(Ctx, E1, int), unify(Ctx, E2, int).
+unify(Ctx, -(E1, E2), int) :- unify(Ctx, E1, int), unify(Ctx, E2, int).
+unify(Ctx, =:=(E1, E2), bool) :- unify(Ctx, E1, T), unify(Ctx, E2, T).
+unify(Ctx, =\=(E1, E2), bool) :- unify(Ctx, E1, T), unify(Ctx, E2, T).
 
-unify(X, T, Ctx) :- atomic(X), member(type(X, T), Ctx).
-unify(abstract(X, E), fn(S, T), Ctx) :-
-	unify(E, T, [type(X, S)|Ctx]).
-unify(apply(E1, E2), T, Ctx) :-
-	unify(E2, S, Ctx),
-	unify(E1, fn(S, T), Ctx).
+% (STLC-3)
+unify(Ctx, abstract(X, E), fn(S, T)) :-
+	unify([type(X, S)|Ctx], E, T).
 
-typeof(X, T) :- unify(X, T, []).
+% (STLC-4)
+unify(Ctx, apply(E1, E2), T) :-
+	unify(Ctx, E2, S),
+	unify(Ctx, E1, fn(S, T)).
+
+typeof(X, T) :- unify([], X, T).
 
 
 test(unify) :-
@@ -24,7 +32,12 @@ test(unify) :-
 	TAs = [int],
 	findall(TR, typeof(abstract(f, apply(f, 1) + 1), TR), TRs),
 	TRs = [fn(fn(int, int), int)],
-	findall(TC, typeof(abstract(x, abstract(y, x + y)), TC), TCs),
-	TCs = [fn(int, fn(int, int))],
+	findall(TC, typeof(abstract(x, abstract(y, x =:= y)), TC), TCs),
+	TCs = [fn(int, fn(int, bool))],
+	true.
+
+test(systemf) :-
+	findall(TF, typeof(abstract(f, apply(f, 1)), TF), TFs),
+	TFs = [fn(fn(int, A), A)], var(A),
 	true.
 

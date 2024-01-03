d74dac062fd4f7eb41c099587c5fd2482159ce58
gmd01bit <gmd01bit@gmail.com>
2024-01-03 17:37:20 +0200

    :memo: Renaming and reorganizing

--- types.pl
+++ types.pl
@@ -1,29 +1,44 @@
+% type.pl
+%! unify(?Ctx:list, +X, -T) is semidet.
+%! typeof(+X, -T) is semidet.
+
+typeof(X, T) :- lambda(X), !, unify([], X, T).
+
 % Simply Typed Lambda Calculus
 % https://en.wikipedia.org/wiki/Simply_typed_lambda_calculus#Typing_rules
-%
-%! unify(?Ctx:list, +X, -T) is semidet.
 
-% (STLC-1)
-unify(Ctx, X, T) :- atomic(X), member(type(X, T), Ctx).
+%% Syntax
+lambda(abstract(X, E)) :- atomic(X), lambda(E).
+lambda(apply(E1, E2)) :- lambda(E1), lambda(E2).
 
-% (STLC-2)
+lambda(+(E1, E2)) :- lambda(E1), lambda(E2).
+lambda(-(E1, E2)) :- lambda(E1), lambda(E2).
+lambda(=:=(E1, E2)) :- lambda(E1), lambda(E2).
+lambda(=\=(E1, E2)) :- lambda(E1), lambda(E2).
+lambda(X) :- atomic(X).
+lambda(X) :- integer(X).
+
+%% (STLC-1)
+unify(Ctx, X, T) :- atomic(X), member(typeis(X, T), Ctx).
+
+%% (STLC-2)
 unify(_Ctx, X, int) :- integer(X).
 unify(Ctx, +(E1, E2), int) :- unify(Ctx, E1, int), unify(Ctx, E2, int).
 unify(Ctx, -(E1, E2), int) :- unify(Ctx, E1, int), unify(Ctx, E2, int).
 unify(Ctx, =:=(E1, E2), bool) :- unify(Ctx, E1, T), unify(Ctx, E2, T).
 unify(Ctx, =\=(E1, E2), bool) :- unify(Ctx, E1, T), unify(Ctx, E2, T).
 
-% (STLC-3)
+%% (STLC-3)
 unify(Ctx, abstract(X, E), fn(S, T)) :-
-	unify([type(X, S)|Ctx], E, T).
+	unify([typeis(X, S)|Ctx], E, T).
 
-% (STLC-4)
+%% (STLC-4)
 unify(Ctx, apply(E1, E2), T) :-
 	unify(Ctx, E2, S),
 	unify(Ctx, E1, fn(S, T)).
 
-typeof(X, T) :- unify([], X, T).
-
+% Tests
+% test(Pass).
 
 test(unify) :-
 	findall(TL, typeof(abstract(x, x + x), TL), TLs),
@@ -32,12 +47,14 @@ test(unify) :-
 	TAs = [int],
 	findall(TR, typeof(abstract(f, apply(f, 1) + 1), TR), TRs),
 	TRs = [fn(fn(int, int), int)],
-	findall(TC, typeof(abstract(x, abstract(y, x =:= y)), TC), TCs),
-	TCs = [fn(int, fn(int, bool))],
+	findall(TC, typeof(abstract(x, abstract(y, x + y)), TC), TCs),
+	TCs = [fn(int, fn(int, int))],
 	true.
 
 test(systemf) :-
+	findall(TE, typeof(abstract(x, abstract(y, x =:= y)), TE), TEs),
+	TEs = [fn(A, fn(A, bool))], var(A),
 	findall(TF, typeof(abstract(f, apply(f, 1)), TF), TFs),
-	TFs = [fn(fn(int, A), A)], var(A),
+	TFs = [fn(fn(int, B), B)], var(B),
 	true.
 

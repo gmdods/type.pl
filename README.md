807179cad0f6d6c388f97c8e4863f32e7e22960b
gmd01bit <gmd01bit@gmail.com>
2024-01-03 02:42:32 +0200

    :sparkles: Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # type.pl
 A type checker in Prolog for typed lambda calculi.
+
+## Reference
+
+- [Simply typed lambda calculus](https://en.wikipedia.org/wiki/Simply_typed_lambda_calculus)
+
+## Influence
+
+- [Simply typed lambda calculus in OCaml](https://www.youtube.com/watch?v=E3BjV-Y6jlk)
---
+++ types.pl
@@ -0,0 +1,24 @@
+% unify Expr is T given Ctx
+
+unify(X, int, _Ctx) :- integer(X).
+
+unify(+(E1, E2), int, Ctx) :- unify(E1, int, Ctx), unify(E2, int, Ctx).
+unify(-(E1, E2), int, Ctx) :- unify(E1, int, Ctx), unify(E2, int, Ctx).
+unify(=:=(E1, E2), bool, Ctx) :- unify(E1, T, Ctx), unify(E2, T, Ctx).
+unify(=\=(E1, E2), bool, Ctx) :- unify(E1, T, Ctx), unify(E2, T, Ctx).
+
+unify(X, T, Ctx) :- atomic(X), member(type(X, T), Ctx).
+unify(abstract(X, E), fn(S, T), Ctx) :-
+	unify(E, T, [type(X, S)|Ctx]).
+unify(apply(E1, E2), T, Ctx) :-
+	unify(E2, S, Ctx),
+	unify(E1, fn(S, T), Ctx).
+
+typeof(X, T) :- unify(X, T, []).
+
+
+test(unify) :-
+	findall(TL, typeof(abstract(x, x + x), TL), TLs),
+	TLs = [fn(int, int)],
+	findall(TA, typeof(apply(abstract(x, x + x), 1), TA), TAs),
+	TAs = [int].
